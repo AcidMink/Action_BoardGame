1b697ed001e1d03f8b9a9a5cb50a6e7c19dc7b91
AcidMink <163882978+AcidMink@users.noreply.github.com>
2024-05-30 12:54:39 +0300

    Create Base.cpp

---
+++ Base.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+#include <list>
+#include <algorithm>
+#include <cstdio>
+#include <iterator>
+#include <sstream>
+
+using namespace std;
+
+
+
+int main() {
+  cout << "Hello World!" << endl;
+}
